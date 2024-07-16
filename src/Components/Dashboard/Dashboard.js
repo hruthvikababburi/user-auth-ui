1b6da3ec37bbc75b0b17e0e84699f366f1537e5c
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-07-16 11:33:53 +0530

    Worked with Dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { MdOutlineDashboardCustomize } from "react-icons/md";
 
+import './Dashboard.css'
 export default function Dashboard() {
   return (
-    <div>
-      This is Dashboard Page
+    <div className='dashboard-cont'>
+      <div className='d-context-cont'>
+        <div className='common-cont'>
+          <h1>Welcome to Your Dashboard</h1>
+          <MdOutlineDashboardCustomize className='icon'/>
+        </div>
+        
+        <h3>Your Personalized Hub for UserVerse</h3>
+        <p>Welcome back, UserVerse! Here on your dashboard, 
+          you have access to all the tools and insights you need 
+          to manage your UserVerse experience effectively.
+        </p>
+        <h3>Your Profile Information</h3>
+          
+        <h5>User Details</h5>
+        <ul>
+          <li>
+            <h5>Name: </h5> 
+            <span>this is name</span>
+          </li>
+          <li>
+            <h5>Email: </h5> 
+            <span>this is </span>
+          </li>
+          <li>
+            <h5>Contact: </h5> 
+            <span>this is </span>
+          </li>
+          <li>
+            <h5>Registration Id: </h5> 
+            <span>this is </span>
+          </li>
+        </ul>
+
+      </div>
     </div>
   )
 }
